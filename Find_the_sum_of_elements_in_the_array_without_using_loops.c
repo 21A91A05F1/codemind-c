843332567de4e8241bfc2dfa99d31f2d0fbdb15f
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-02-12 19:54:39 +0530

    #include<stdio.h> int main() {     int n,i,sum=0,arr[10000];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     printf("%d",sum); }

---
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0,arr[10000];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+}
