28b21d43711e80f2e49b17601e4edfed594dde06
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-02-19 11:51:46 +0530

    #include<stdio.h> #include<math.h> int main() {     int T,i,n,q;     float s,res;     scanf("%d",&T);     for(i=0;i<T;i++)     {             scanf("%d",&n);
            s=sqrt(n);
            res=(int)(s);
        if(s==res)
        {
            printf("True
    ");
        }
        else
        {
            printf("False
    ");
        }
        }
        
        
    }

---
+++ Valid_Perfect_Square.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int T,i,n,q;
+    float s,res;
+    scanf("%d",&T);
+    for(i=0;i<T;i++)
+    {
+            scanf("%d",&n);
+
+        s=sqrt(n);
+        res=(int)(s);
+    if(s==res)
+    {
+        printf("True
+");
+    }
+    else
+    {
+        printf("False
+");
+    }
+    }
+    
+    
+}
