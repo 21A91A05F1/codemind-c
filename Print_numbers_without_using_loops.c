5f2f175e085b828c5c005c7a0a170c206c0e3816
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-03-05 11:25:18 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     if(n >=1 && n<=10000)     {         for(i=1;i<=n;i++)         {             printf("%d ",i);         }     }     else     {         printf("The Number Series is Not Possible Print");     } }

---
+++ Print_numbers_without_using_loops.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n >=1 && n<=10000)
+    {
+        for(i=1;i<=n;i++)
+        {
+            printf("%d ",i);
+        }
+    }
+    else
+    {
+        printf("The Number Series is Not Possible Print");
+    }
+}
