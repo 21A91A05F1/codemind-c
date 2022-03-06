1aba225713a75a659390dd8fccc54f148c2226ab
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-03-06 18:53:20 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
-   
         for(j=1;j<=i;j++)
         {
-         printf("%d",j);
+            printf("%d",j);
         }
         printf("
 ");
     }
-    return 0;
 }
