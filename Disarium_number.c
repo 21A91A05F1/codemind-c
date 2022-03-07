ab3d503b66a032f02d0cec94c8a8d66ba8583d71
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-03-07 21:26:24 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,temp=0,d,d1,rev=0,res=0,c=0;     scanf("%d",&n);     temp=n;     while(n>0)    {     d=n%10;     rev=rev*10+d;     n=n/10;   }   n=temp;   while(rev>0)   {       d1=rev%10;       c++;       res=res+pow(d1,c);       rev=rev/10;   }   if(res==temp)   {       printf("True");   }   else   {       printf("False");   } }

---
+++ Disarium_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,temp=0,d,d1,rev=0,res=0,c=0;
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+   {
+    d=n%10;
+    rev=rev*10+d;
+    n=n/10;
+  }
+  n=temp;
+  while(rev>0)
+  {
+      d1=rev%10;
+      c++;
+      res=res+pow(d1,c);
+      rev=rev/10;
+  }
+  if(res==temp)
+  {
+      printf("True");
+  }
+  else
+  {
+      printf("False");
+  }
+}
