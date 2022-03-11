3eafd56fa21c95cf65a6d62229d0d09385ce84a8
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-03-11 10:08:17 +0530

    #include<stdio.h> #include<ctype.h> int main() {     char str[100],ch;     int i;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>=65 && str[i]<=90)         {             str[i]=str[i]+32;         }         printf("%c",str[i]);     }
    }

---
+++ To_Lower_Case.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char str[100],ch;
+    int i;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>=65 && str[i]<=90)
+        {
+            str[i]=str[i]+32;
+        }
+        printf("%c",str[i]);
+    }
+   
+    
+    
+}
