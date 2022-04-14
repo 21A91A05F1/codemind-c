07d8da9bf9521e1b86e89b2eff0afb0e603c8381
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-04-14 18:21:34 +0530

    #include<stdio.h> int main() {     int n,i,j,d[100],flag=0,c=0,k=0;     scanf("%d",&n);     c=0;     while(n>0)     {         d[i++]=n%10;         c++;         n=n/10;     }     for(i=0;i<c;i++)     {flag=0;         for(j=0;j<c;j++)         {             if(d[i]==d[j] && i!=j && d[i]>0)             {                 flag=1;                 d[j]=-1;                 break;             }         }         if(flag==1)         {
                printf("Not Unique Number");
                return 0;
            }
        }
        printf("Unique Number");
        return 0;
        
    
        
    }

--- Unique_number.c
+++ Unique_number.c
@@ -1,33 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,c=0,d[100],flag=0;
+    int n,i,j,d[100],flag=0,c=0,k=0;
     scanf("%d",&n);
+    c=0;
     while(n>0)
-    {
+    { 
         d[i++]=n%10;
         c++;
         n=n/10;
     }
     for(i=0;i<c;i++)
-    {
-        flag=0;
+    {flag=0;
         for(j=0;j<c;j++)
         {
-             if(d[i]==d[j] && i!=j )
+            if(d[i]==d[j] && i!=j && d[i]>0)
             {
                 flag=1;
-                 break;
+                d[j]=-1;
+                break;
             }
-       
-         }
-    if(flag==1)
-    {
-        printf("Not Unique Number");
-        return 0;
+        }
+        if(flag==1)
+        {
+
+            printf("Not Unique Number");
+            return 0;
+        }
     }
-    }
-    
     printf("Unique Number");
     return 0;
+    
+
+    
 }
