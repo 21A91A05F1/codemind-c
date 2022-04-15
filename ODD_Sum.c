4d32259594236cb89948ebc98fe9d4e06dcbc786
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-04-15 10:11:31 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         sum=sum+arr[i];     }     printf("%d",sum); }

---
+++ ODD_Sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+}
