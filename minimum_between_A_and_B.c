8da459f4d3e9af4d1ce5116d8a1a700e9030913a
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-04-16 14:53:02 +0530

    #include<stdio.h> int main() {     int a,b,arr[100],i,n,sum=0,min,k=0,ar[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {
            if(arr[i]>=a && arr[i]<=b)
            {
                c++;
                ar[k]=arr[i];
                k++;
            }
        }
          if(c==0)
        printf("-1");
        else
        {
            min=ar[0];
            for(k=0;k<c;k++)
            {
                if(ar[k]<min)
                min=ar[k];
            }
            printf("%d",min);
        }
      
    }

---
+++ minimum_between_A_and_B.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,arr[100],i,n,sum=0,min,k=0,ar[100],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            c++;
+            ar[k]=arr[i];
+            k++;
+        }
+    }
+      if(c==0)
+    printf("-1");
+    else
+    {
+        min=ar[0];
+        for(k=0;k<c;k++)
+        {
+            if(ar[k]<min)
+            min=ar[k];
+        }
+        printf("%d",min);
+    }
+  
+}
