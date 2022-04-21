ebe1a9ae0965e688464b687999219dc426a1832d
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-04-21 20:37:07 +0530

    #include<stdio.h> int main() {     int n,arr[10000],a1[100000],i,d=0,rev=0,temp,c=0;     scanf("%d",&n);
        for(i=0;i<n;i++)
        scanf("%d",&arr[i]);
        for(i=0;i<n;i++)
        {
            rev=0;
            d=0;
            temp=arr[i];
            while(arr[i]>0)
            {
                d=arr[i]%10;
                rev=rev*10+d;
                arr[i]=arr[i]/10;
            }
             printf("%d ",rev);
        }
        
        
    }

---
+++ Reverse_game.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[10000],a1[100000],i,d=0,rev=0,temp,c=0;
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<n;i++)
+    {
+        rev=0;
+        d=0;
+        temp=arr[i];
+        while(arr[i]>0)
+        {
+            d=arr[i]%10;
+            rev=rev*10+d;
+            arr[i]=arr[i]/10;
+        }
+         printf("%d ",rev);
+    }
+    
+    
+}
