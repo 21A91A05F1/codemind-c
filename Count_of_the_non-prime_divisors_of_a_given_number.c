ac24767f05100e2ad4f6938f041a8448816be013
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-04-23 11:48:27 +0530

    #include<stdio.h> int main() {     int n,c=0,i,j,d=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {c=0;             for(j=1;j<=n;j++)             {                 if(i%j==0)                 {                     c++;                 }             }             if(c==2)             {                 continue;             }             else             {                 d++;             }         }     }     printf("%d",d);
    }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,c=0,d=0;
+    int n,c=0,i,j,d=0;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         if(n%i==0)
-        {  c=0;
+        {c=0;
             for(j=1;j<=n;j++)
             {
                 if(i%j==0)
@@ -14,11 +14,16 @@ int main()
                     c++;
                 }
             }
-            if(c!=2)
+            if(c==2)
+            {
+                continue;
+            }
+            else
             {
                 d++;
             }
         }
     }
     printf("%d",d);
+
 }
