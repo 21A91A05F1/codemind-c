5cdf1649c086834e2316a2fa4348e32bcdf35c33
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-04-30 06:22:25 +0530

    #include<stdio.h> int main() {     int n,arr[10000],i,d=0,flag=0,j,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {        // d=0;         flag=1;         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j] && i!=j  )             {                 flag++;                 arr[j]=-1;
                }
            }
           if(arr[i]!=-1)
           {
               printf("%d %d ",arr[i],flag);
           }
         }
        // printf("%d",s);
        
    }

---
+++ Element_and_Count.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[10000],i,d=0,flag=0,j,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       // d=0;
+        flag=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j] && i!=j  )
+            {
+                flag++;
+                arr[j]=-1;
+               
+            }
+        }
+       if(arr[i]!=-1)
+       {
+           printf("%d %d ",arr[i],flag);
+       }
+     }
+    // printf("%d",s);
+    
+}
+
