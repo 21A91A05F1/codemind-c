9f14b69e3633b68f2ef2dfa41842e16199bebbe8
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-04-30 06:18:45 +0530

    #include<stdio.h> int main() {     int n,arr[10000],i,d=0,flag=0,j,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {        // d=0;         flag=1;         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j] && i!=j  )             {                 flag++;
                }
            }
           if(flag==1)
           {
               s=s+arr[i];
           }
         }
         printf("%d",s);
        
    }

---
+++ unique_sum.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[10000],i,d=0,flag=0,j,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       // d=0;
+        flag=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j] && i!=j  )
+            {
+                flag++;
+               
+            }
+        }
+       if(flag==1)
+       {
+           s=s+arr[i];
+       }
+     }
+     printf("%d",s);
+    
+}
+
